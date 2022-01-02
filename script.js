163870cd7bd9168d862ebd3a55c17c9d4ac680d6
Cameron Chandler <webstormcam@gmail.com>
2022-01-02 08:44:01 -0500

    Made the drop down work

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let ham = document.getElementById('ham');
+let drop = document.getElementById('dropdown')
 
 
 function clickHam(){
 if(ham.getAttribute('src') ==='imgs/icon-close.svg'){
     ham.src = 'imgs/icon-hamburger.svg'
+    drop.style.display='none'
  
 } else{
     ham.src = 'imgs/icon-close.svg'
+    drop.style.display='flex'
     
 }
 }
